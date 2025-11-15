703b4eaddf88f67705dc7bce9fdcfe5ab42fbdd0
Yasineert <yassineouhadi99@gmail.com>
2025-11-15 17:43:40 +0100

    tp7

---
+++ src/main/java/com/example/jaxrs/entite/Compte.java
@@ -0,0 +1,34 @@
+package com.example.jaxrs.entite;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Compte {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double solde;
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+    @Enumerated(EnumType.ORDINAL)
+    private TypeCompte type;
+
+    public enum TypeCompte {
+        COURANT, EPARGNE
+    }
+}
---
+++ src/main/java/com/example/jaxrs/entite/CompteRepository.java
@@ -0,0 +1,5 @@
+package com.example.jaxrs.entite;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompteRepository extends JpaRepository<Compte,Long>{}
